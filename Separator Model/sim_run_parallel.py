a56d47b2a4708348487b3a4eecb888daccd9be81
iagr98 <ivan.rivillas@correounivalle.edu.co>
2025-05-29 10:28:00 +0200

    Changes made to parallel code

--- Separator Model/sim_run_parallel.py
+++ Separator Model/sim_run_parallel.py
@@ -3,25 +3,28 @@ import pandas as pd
 import joblib
 from sim_run import run_sim
 
-N_CPU = 4
-N_D_value = 15
+N_CPU = 7
+N_D = 15
 filename = "Paraffin_flut_20C.xlsx"
-s_value = 1.25 
-N_x_values = [61, 71, 81, 91]
+N_x = 101
+atol = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3]
+
+var = 'atol'
+var_num = {'atol': atol, 'N_x': N_x, 'N_D': N_D}.get(var)
 
 
-def parallel_simulation(params): # Quasi "wrapper function from sozh"
-    N_D, filename, s, N_x = params
-    print(f"Start simulation with N_x={N_x}")
+def parallel_simulation(params):
+    N_D, filename, N_x, atol = params
+    print(f"Start simulation with {var}={var_num}")
     try:
-        Sim = run_sim(filename, s=s, N_D=N_D, N_x=N_x)
-        return {'N_x': N_x, 'V_dis_total': Sim.V_dis_total, 'status': 'success'}
+        Sim = run_sim(filename, N_D=N_D, N_x=N_x, a_tol=atol)
+        return {f"{var}": var_num, 'V_dis_total': Sim.V_dis_total, 'status': 'success'}
     except Exception as e:
-        print(f"Simulation failed by N_D={N_D}: {str(e)}")
-        return {'N_x': N_x, 'error': str(e), 'status': 'failed'}
+        print(f"Simulation failed by {var}={var_num}: {str(e)}")
+        return {f"{var}": var_num, 'error': str(e), 'status': 'failed'}
 
 if __name__ == "__main__":
-    parameters = [(N_D_value, filename, s_value, N_x) for N_x in N_x_values]
+    parameters = [(N_D, filename, N_x, atol_value) for atol_value in atol]
     
     results = joblib.Parallel(n_jobs=N_CPU)(joblib.delayed(parallel_simulation)(param) for param in parameters)
     
