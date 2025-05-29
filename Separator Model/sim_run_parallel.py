8f46a37f0e2575db58ee3ff2c9f2a02ee70be9d3
iagr98 <ivan.rivillas@correounivalle.edu.co>
2025-05-29 10:45:35 +0200

    Last changes to parallel code

--- Separator Model/sim_run_parallel.py
+++ Separator Model/sim_run_parallel.py
@@ -3,28 +3,27 @@ import pandas as pd
 import joblib
 from sim_run import run_sim
 
-N_CPU = 7
+N_CPU = 4
 N_D = 15
 filename = "Paraffin_flut_20C.xlsx"
 N_x = 101
 atol = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3]
 
 var = 'atol'
-var_num = {'atol': atol, 'N_x': N_x, 'N_D': N_D}.get(var)
 
 
 def parallel_simulation(params):
     N_D, filename, N_x, atol = params
-    print(f"Start simulation with {var}={var_num}")
+    print(f"Start simulation with {var}={atol}")                                        # Update parameter in second {}
     try:
         Sim = run_sim(filename, N_D=N_D, N_x=N_x, a_tol=atol)
-        return {f"{var}": var_num, 'V_dis_total': Sim.V_dis_total, 'status': 'success'}
+        return {f"{var}": atol, 'V_dis_total': Sim.V_dis_total, 'status': 'success'}    # Update parameter in second place
     except Exception as e:
-        print(f"Simulation failed by {var}={var_num}: {str(e)}")
-        return {f"{var}": var_num, 'error': str(e), 'status': 'failed'}
+        print(f"Simulation failed by {var}={atol}: {str(e)}")                           # Update parameter in second {}
+        return {f"{var}": atol, 'error': str(e), 'status': 'failed'}                    # Update parameter in second place
 
 if __name__ == "__main__":
-    parameters = [(N_D, filename, N_x, atol_value) for atol_value in atol]
+    parameters = [(N_D, filename, N_x, atol_value) for atol_value in atol]              # Update parameter var_value, var_value & var 
     
     results = joblib.Parallel(n_jobs=N_CPU)(joblib.delayed(parallel_simulation)(param) for param in parameters)
     
