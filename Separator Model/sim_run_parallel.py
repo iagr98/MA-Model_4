3b2e683d8c69b6a189bceb1d1ec6422ff25dcee2
Ivan Guzman <ivgu01@avt.rwth-aachen.de>
2025-05-30 15:22:05 +0200

    Parallel code ready for tolerance analysis

--- Separator Model/sim_run_parallel.py
+++ Separator Model/sim_run_parallel.py
@@ -6,27 +6,27 @@ import helper_functions as hf
 from sim_run import run_sim
 
 N_CPU = 8
-N_D = [10, 13, 15, 17, 20, 30, 50, 60]
+N_D = 30
 filename = "Paraffin_flut_20C.xlsx"
 # filename = "niba_V2.xlsx"
 N_x = 151
-atol = 1e-6
+atol = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9]
 
-var = 'N_D'             # Define
+var = 'atol'             # Define
 
 
 def parallel_simulation(params):
     N_D, filename, N_x, atol = params
-    print(f"Start simulation with {var}={N_D}")                                        # Update parameter in second {}
+    print(f"Start simulation with {var}={atol}")                                        # Update parameter in second {}
     try:
         Sim = run_sim(filename, N_D=N_D, N_x=N_x, a_tol=atol)
-        return {f"{var}": N_D, 'Sep. Eff.': Sim.E,'Vol_imbalance [%]': hf.calculate_volume_balance(Sim), 'status': 'success'}    # Update parameter in second place
+        return {f"{var}": atol, 'V_dis_total': Sim.V_dis_total, 'Sep. Eff.': Sim.E,'Vol_imbalance [%]': hf.calculate_volume_balance(Sim), 'status': 'success'}    # Update parameter in second place
     except Exception as e:
-        print(f"Simulation failed by {var}={N_D}: {str(e)}")                           # Update parameter in second {}
-        return {f"{var}": N_D, 'error': str(e), 'status': 'failed'}                    # Update parameter in second place
+        print(f"Simulation failed by {var}={atol}: {str(e)}")                           # Update parameter in second {}
+        return {f"{var}": atol, 'error': str(e), 'status': 'failed'}                    # Update parameter in second place
 
 if __name__ == "__main__":
-    parameters = [(N_D_value, filename, N_x, atol) for N_D_value in N_D]              # Update parameter var_value, var_value & var 
+    parameters = [(N_D, filename, N_x, atol_value) for atol_value in atol]              # Update parameter var_value, var_value & var 
     
     results = joblib.Parallel(n_jobs=N_CPU)(joblib.delayed(parallel_simulation)(param) for param in parameters)
     
@@ -39,10 +39,12 @@ if __name__ == "__main__":
     df = pd.read_csv("simulation_results_parallel.csv")
     df.columns = df.columns.str.strip()
     plt.figure(figsize=(8, 5))
-    plt.plot(df['N_D'], df['Sep. Eff.'], marker='o')         # Update parameter in first place
-    # plt.xscale('log')  # da atol logarithmisch skaliert ist
-    plt.xlabel('N_D')                                          # Change x-label
-    plt.ylabel('Sep. Eff.')
+    # plt.plot(df['atol'], df['Sep. Eff.'], marker='o')         # Update parameter in first place
+    plt.plot(df['atol'], df['V_dis_total'], marker='o')         # Update parameter in first place
+    plt.xscale('log')  # da atol logarithmisch skaliert ist
+    plt.yscale('log')  # da atol logarithmisch skaliert ist
+    plt.xlabel('atol')                                          # Change x-label
+    plt.ylabel('V_dis')                                         # Change output variable if needed
     plt.title(f'Gitterunabhängigkeitsanalyse ({var})')
     plt.grid(True)
     plt.tight_layout()
