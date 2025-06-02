7bba0a1864b01638500ab00bb207c127b76d58ce
iagr98 <ivan.rivillas@correounivalle.edu.co>
2025-06-02 07:25:07 +0200

    Re-calculation of V_dis_ye to 2L_DPZ/3

--- Separator Model/comparison.py
+++ Separator Model/comparison.py
@@ -4,8 +4,8 @@ import numpy as np
 
 df_sim = pd.read_csv("Output/simulation_results_parallel_evaluation_cos.csv")
 df_lab = pd.read_excel("Input/data_main.xlsx", sheet_name="main")
-extra = 0.004   # X-Y limit of plot
-data = 34       # [0:34]: Only considers ye data, [0,-1] all data.
+extra = 0.0085   # X-Y limit of plot
+data = -1       # [0:34]: Only considers ye data, [0,-1] all data.
 
 V_dis_0 = np.concatenate([[0], df_lab['V_dis'][0:data], [extra]])
 V_dis_plus_25 = V_dis_0 * 1.25
@@ -22,11 +22,9 @@ plt.plot(V_dis_0, V_dis_0, color='black', linestyle='--')
 plt.scatter(df_lab['V_dis'], df_sim['V_dis_total'], label=f'Modell 4. MAPE: {mape_sim:.2f}%. std. Abw.: {mape_std_sim:.2f}%', color='black')
 df_sim_ratio_1 = []
 df_sim_ratio_2 = []
-df_sim_ratio_3 = []
 df_sim_niba = []
 df_lab_ratio_1 = []
 df_lab_ratio_2 = []
-df_lab_ratio_3 = []
 df_lab_niba = []
 for i in range(len(V_dis_0)-1):
     if (df_lab['ratio'][i] == 0):
@@ -35,15 +33,11 @@ for i in range(len(V_dis_0)-1):
     if (df_lab['ratio'][i] < 0.1):
         df_sim_ratio_1.append(df_sim['V_dis_total'][i])
         df_lab_ratio_1.append(df_lab['V_dis'][i])
-    elif(0.1 < df_lab['ratio'][i] < 0.6):
+    elif(0.1 < df_lab['ratio'][i]):
         df_sim_ratio_2.append(df_sim['V_dis_total'][i])
         df_lab_ratio_2.append(df_lab['V_dis'][i])
-    elif(df_lab['ratio'][i] > 0.6): 
-        df_sim_ratio_3.append(df_sim['V_dis_total'][i])
-        df_lab_ratio_3.append(df_lab['V_dis'][i])
 plt.scatter(df_lab_ratio_1, df_sim_ratio_1, label='ratio < 0.1', color='red')
-plt.scatter(df_lab_ratio_2, df_sim_ratio_2, label='0.1 < ratio < 0.6', color='green')
-plt.scatter(df_lab_ratio_3, df_sim_ratio_3, label='ratio > 0.6', color='blue')
+plt.scatter(df_lab_ratio_2, df_sim_ratio_2, label='0.1 < ratio', color='green')
 plt.scatter(df_lab_niba, df_sim_niba, label='niba', color='orange')
 plt.plot(V_dis_0, V_dis_plus_25, color='gray', linestyle=':', alpha=0.5)
 plt.plot(V_dis_0, V_dis_minus_25, color='gray', linestyle=':', alpha=0.5)
