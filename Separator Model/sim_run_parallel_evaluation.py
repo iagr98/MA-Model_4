c79da55159edc19b15cfcb7e39e6400de5299003
iagr98 <ivan.rivillas@correounivalle.edu.co>
2025-07-25 18:15:38 +0200

    Updated parallel code for running lambda detail tests. Also added lamdda detail sheet to data_main.xlsx in the Input folder.

--- Separator Model/sim_run_parallel_evaluation.py
+++ Separator Model/sim_run_parallel_evaluation.py
@@ -8,7 +8,7 @@ import csv
 
 N_CPU = 8
 
-experiment = "detail_V_dis" # "main" if ye + niba tests, "sozh" tests from AVT.FVT
+experiment = "detail_lambda" # "main" if ye + niba tests, "sozh" tests from AVT.FVT
 
 df = pd.read_excel("Input/data_main.xlsx", sheet_name=experiment)
 exp = df['exp'].tolist()
@@ -22,14 +22,14 @@ if (experiment == "sozh" or experiment == "detail_V_dis"):
 
 
 def parallel_simulation(params):
-    if (experiment == "main"):    
+    if (experiment == "main" or experiment == 'detail_lambda'):    
         exp, phi_0, dV_ges, eps_0 = params
         print(f"Start simulation with exp={exp}, phi_0={phi_0}, dV_ges={dV_ges}, eps_0={eps_0}")
     elif(experiment == "sozh" or experiment == "detail_V_dis"):
         exp, phi_0, dV_ges, eps_0, h_d_0, h_dis_0 = params
         print(f"Start simulation with exp={exp}, phi_0={phi_0}, dV_ges={dV_ges}, eps_0={eps_0}, h_d_0={h_d_0}, h_dis_0={h_dis_0}")
     try:
-        if (experiment == "main"):
+        if (experiment == "main" or experiment == 'detail_lambda'):
             Sim = run_sim(exp, phi_0, dV_ges, eps_0)
             result = {'exp': exp, 'phi_0': phi_0, 'dV_ges': dV_ges, 'eps_0': eps_0,
                     'V_dis_total': Sim.V_dis_total, 'Sep. Eff.': Sim.E,
@@ -41,32 +41,32 @@ def parallel_simulation(params):
                 'V_dis_total': Sim.V_dis_total, 'Sep. Eff.': Sim.E,
                 'Vol_imbalance [%]': hf.calculate_volume_balance(Sim), 'status': 'success'}  
             
-            # Expand h_d and h_dpz arrays into separate columns
-            for i, val in enumerate(Sim.h_d):
-                result[f'h_d_{i}'] = val
-            for i, val in enumerate(Sim.h_dpz):
-                result[f'h_dpz_{i}'] = val
+        # Expand h_d and h_dpz arrays into separate columns
+        for i, val in enumerate(Sim.h_d):
+            result[f'h_d_{i}'] = val
+        for i, val in enumerate(Sim.h_dpz):
+            result[f'h_dpz_{i}'] = val
             
          # Schreibe das Ergebnis sofort in die CSV-Datei
-        with open('simulation_results_parallel_evaluation_detail.csv', mode='a', newline='') as file:
+        with open('simulation_results_parallel_evaluation_detail_lambda.csv', mode='a', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=result.keys())
             writer.writerow(result)
         return result 
     except Exception as e:
-        if (experiment == "main"):
+        if (experiment == "main" or experiment == 'detail_lambda'):
             print(f"Simulation failed for exp={exp}, phi_0={phi_0}, dV_ges={dV_ges}, eps_0={eps_0}: {str(e)}")
             error_result = {'exp': exp, 'phi_0': phi_0, 'dV_ges': dV_ges, 'eps_0': eps_0, 'error': str(e), 'status': 'failed'}
         elif(experiment == "sozh" or experiment == "detail_V_dis"):
             print(f"Simulation failed for exp={exp}, phi_0={phi_0}, dV_ges={dV_ges}, eps_0={eps_0}, h_d_0={h_d_0}, h_dis_0={h_dis_0}: {str(e)}")
             error_result = {'exp': exp, 'phi_0': phi_0, 'dV_ges': dV_ges, 'eps_0': eps_0, 'h_d_0': h_d_0, 'h_dis_0': h_dis_0, 'error': str(e), 'status': 'failed'}
 
-        with open('simulation_results_parallel_evaluation_detail.csv', mode='a', newline='') as file:
+        with open('simulation_results_parallel_evaluation_detail_lambda.csv', mode='a', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=error_result.keys())
             writer.writerow(error_result)
         return error_result
 
 if __name__ == "__main__":
-    if (experiment == "main"):
+    if (experiment == "main" or experiment == 'detail_lambda'):
         parameters = [(exp[i], phi_0[i], dV_ges[i], eps_0[i]) for i in range(len(exp))]
     elif(experiment == "sozh" or experiment == "detail_V_dis"):
         parameters = [(exp[i], phi_0[i], dV_ges[i], eps_0[i], h_d_0[i], h_dis_0[i]) for i in range(len(exp))]
@@ -77,17 +77,17 @@ if __name__ == "__main__":
     h_dpz_fields = [f'h_dpz_{i}' for i in range(n)]
     all_fields = base_fields + h_d_fields + h_dpz_fields
     # Header der CSV-Datei schreiben, falls sie noch nicht existiert
-    with open('simulation_results_parallel_evaluation_detail.csv', mode='w', newline='') as file:
+    with open('simulation_results_parallel_evaluation_detail_lambda.csv', mode='w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=all_fields)
         writer.writeheader()
 
     results = joblib.Parallel(n_jobs=N_CPU, backend='multiprocessing')(joblib.delayed(parallel_simulation)(param) for param in parameters)
 
     # Save results
-    print("Alle Simulationen abgeschlossen. Ergebnisse gespeichert.")
     df_results = pd.DataFrame(results)
     # h_dpz_columns = pd.DataFrame(df_results['h_dpz'].tolist())   # Convert h_dpz (list of arrays) into separate columns
     # h_dpz_columns.columns = [f'h_dpz_{i}' for i in range(h_dpz_columns.shape[1])]
     # df_results = df_results.drop(columns=['h_dpz'])
     # df_results = pd.concat([df_results, h_dpz_columns], axis=1)  # Concatenate V_dis columns with the main result dataframe
-    df_results.to_csv('simulation_results_parallel_evaluation_detail_1.csv', index=False)
+    df_results.to_csv('simulation_results_parallel_evaluation_detail_lambda_1.csv', index=False)
+    print("Alle Simulationen abgeschlossen. Ergebnisse gespeichert.")
